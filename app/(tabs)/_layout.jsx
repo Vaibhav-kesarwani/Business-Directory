1c4509de1f7002e817f02793540d9813aa395f57
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-07-17 09:50:21 +0530

    added the primary colors

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -2,10 +2,11 @@ import { View, Text } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
+import { Colors } from './../../constants/Colors';
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
+    <Tabs screenOptions={{ headerShown: false, tabBarActiveTintColor: Colors.PRIMARY}}>
       <Tabs.Screen
         name="home"
         options={{
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,7 +3,7 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
+const tintColorLight = '#7f57f1';
 const tintColorDark = '#fff';
 
 export const Colors = {
@@ -23,4 +23,6 @@ export const Colors = {
     tabIconDefault: '#9BA1A6',
     tabIconSelected: tintColorDark,
   },
+  PRIMARY:'#7f57f1'
+
 };
