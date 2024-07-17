6012f34b7c176ff05328a2b0abf0f88983746188
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-07-17 09:25:30 +0530

    created the tab files

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function TabLayout() {
+  return (
+    <View>
+      <Text>TabLayout</Text>
+    </View>
+  )
+}
