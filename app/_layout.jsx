392bba61f4a487bafede8a7a88499a67fc85a8a5
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-07-28 10:42:26 +0530

    inti commit

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,7 +1,7 @@
 import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
-import { Text} from 'react-native'
+import LoginScreen from "./../components/LoginScreen";
 
 export default function RootLayout() {
   useFonts({
@@ -19,7 +19,7 @@ export default function RootLayout() {
         </Stack>
       </SignedIn>
       <SignedOut>
-        <Text>SignOut</Text>
+        <LoginScreen />
       </SignedOut>
     </ClerkProvider>
   );
