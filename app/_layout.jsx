22316716eb246c24ed4b0921d0324d3974b94e41
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-07-28 10:33:30 +0530

    remove all the external component files

---
+++ app/_layout.jsx
@@ -0,0 +1,26 @@
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import { Text} from 'react-native'
+
+export default function RootLayout() {
+  useFonts({
+    outfit: require("./../assets/fonts/Outfit-Regular.ttf"),
+    "outfit-medium": require("./../assets/fonts/Outfit-Medium.ttf"),
+    "outfit-bold": require("./../assets/fonts/Outfit-Bold.ttf"),
+  });
+  return (
+    <ClerkProvider
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <SignedIn>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+      </SignedIn>
+      <SignedOut>
+        <Text>SignOut</Text>
+      </SignedOut>
+    </ClerkProvider>
+  );
+}
