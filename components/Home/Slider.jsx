ba59ced4ecba308f988206942af08470e890479a
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-07-30 20:38:21 +0530

    added the function for the slider to list down and fetch the data from the firebase

--- components/Home/Slider.jsx
+++ components/Home/Slider.jsx
@@ -1,10 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { collection, getDocs, query } from "firebase/firestore";
+import { db } from "../../configs/FirebaseConfig";
 
 export default function Slider() {
+  const [sliderList, setSliderList] = useState([]);
+
+  useEffect(() => {
+    GetSliderList();
+  }, []);
+
+  const GetSliderList = async () => {
+    setSliderList([]);
+    const q = query(collection(db, "Slider"));
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach((doc) => {
+      console.log(doc.data());
+      setSliderList((prev) => [...prev, doc.data()]);
+    });
+  };
+
   return (
     <View>
-      <Text>Slider</Text>
+      
     </View>
-  )
+  );
 }
