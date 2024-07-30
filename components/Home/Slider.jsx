6a1010c6b9cab884405a4e8bc030e18e157aacc8
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-07-30 20:45:24 +0530

    Home Screen (Sliderbar)

--- components/Home/Slider.jsx
+++ components/Home/Slider.jsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList, Image } from "react-native";
 import React, { useEffect, useState } from "react";
 import { collection, getDocs, query } from "firebase/firestore";
 import { db } from "../../configs/FirebaseConfig";
@@ -23,7 +23,34 @@ export default function Slider() {
 
   return (
     <View>
-      
+      <Text
+        style={{
+          fontFamily: "outfit-bold",
+          fontSize: 20,
+          paddingLeft: 20,
+          paddingTop: 20,
+          marginBottom: 5,
+        }}
+      >
+        #Special for you
+      </Text>
+      <FlatList
+        data={sliderList}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        style={{ paddingLeft: 20 }}
+        renderItem={({ item, index }) => (
+          <Image
+            source={{ uri: item.imageUrl }}
+            style={{
+              width: 300,
+              height: 150,
+              borderRadius: 15,
+              marginRight: 15,
+            }}
+          />
+        )}
+      />
     </View>
   );
 }
