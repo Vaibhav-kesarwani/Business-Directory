5a568a7f3ac6ad6dd60bb692572e28bc89ee76bc
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-08-01 08:33:56 +0530

    created the popular business card components

--- components/Home/PopularBusiness.jsx
+++ components/Home/PopularBusiness.jsx
@@ -1,10 +1,56 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Colors } from "../../constants/Colors";
+import { collection, getDocs, limit, query } from "firebase/firestore";
+import { db } from "../../configs/FirebaseConfig";
+import PopularBusinessCard from "./PopularBusinessCard";
 
 export default function PopularBusiness() {
+  const [businessList, setBusinessList] = useState([]);
+
+  useEffect(() => {
+    GetBusinessList();
+  }, []);
+
+  const GetBusinessList = async () => {
+    const q = query(collection(db, "BusinessList"), limit(10));
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach((doc) => {
+      console.log(doc.data());
+      setBusinessList((prev) => [...prev, doc.data()]);
+    });
+  };
+
   return (
     <View>
-      <Text>PopularBusiness</Text>
+      <View
+        style={{
+          padding: 20,
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          marginTop: 10,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 20,
+            fontFamily: "outfit-bold",
+          }}
+        >
+          Popular Business
+        </Text>
+        <Text style={{ color: Colors.PRIMARY, fontFamily: "outfit-medium" }}>
+          View All
+        </Text>
+      </View>
+      <FlatList
+        data={businessList}
+        renderItem={({item, index}) => (
+          <PopularBusinessCard business={item} />
+        )}
+      />
     </View>
-  )
+  );
 }
